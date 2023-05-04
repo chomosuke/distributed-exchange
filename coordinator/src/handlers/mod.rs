ac58c64ed64d385d7e9f2bf2eaf556d7e3c6acee
chomosuke <a13323600@gmail.com>
2023-05-05 02:52:04 +1000

    coordinator respond bad request

--- coordinator/src/handlers/mod.rs
+++ coordinator/src/handlers/mod.rs
@@ -16,6 +16,7 @@ pub async fn handler(mut rw: ReadWriter<'_>, global: Arc<Global>) -> Result<Stri
     } else if let Ok(first_line) = node::FirstLine::from_str(&first_line) {
         node::handler(first_line, rw, global).await
     } else {
+        rw.write_line("bad request").await?;
         Err(format!("{first_line} is not a valid request").into())
     }
 }
