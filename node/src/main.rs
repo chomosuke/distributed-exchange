45299ec1593d61d7444824ccfdd95076569994b4
chomosuke <a13323600@gmail.com>
2023-05-04 12:58:54 +1000

    StructOpt for node

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,30 +1,41 @@
-use serde::{Deserialize, Serialize};
-use std::env::args;
-use std::io::{BufRead, BufReader, BufWriter, Write};
-use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::io::{BufRead, BufReader};
+use std::net::{TcpStream, SocketAddr};
+use structopt::StructOpt;
 
-fn main() {
-    contact_coordinator();
+
+#[derive(StructOpt)]
+struct Args {
+    #[structopt(short, long)]
+    coordinator: SocketAddr,
+
+    #[structopt(short, long)]
+    port: u16,
 }
 
-fn contact_coordinator() {
-    println!("Connecting to coordinator server at IP {}, Port {}", "127.0.0.1", "8000");
+fn main() {
+    let args = Args::from_args();
+
+    println!("Contacting coordinator on {}", args.coordinator);
 
     // Connect to coordinator
-    let stream = TcpStream::connect("127.0.0.1:8000").ok()
-        .expect("Failed to connect");
+    let stream = TcpStream::connect(args.coordinator).expect("Failed to connect");
     let mut reader = BufReader::new(&stream);
     let mut buffer: Vec<u8> = Vec::new();
 
     // Get Id from coordinator
-    reader.read_until(b'\n', &mut buffer).ok().expect("Error reading");
-    let buffer_string: String = String::from_utf8(buffer).ok()
+    reader
+        .read_until(b'\n', &mut buffer)
+        .ok()
+        .expect("Error reading");
+    let buffer_string: String = String::from_utf8(buffer)
+        .ok()
         .expect("Error unwrapping string");
     let buffer_string: String = format!("{}", buffer_string.trim_end());
 
-    let server_id: i32 = buffer_string.parse::<i32>().ok()
+    let server_id: i32 = buffer_string
+        .parse::<i32>()
+        .ok()
         .expect("Error parsing server id number");
 
     println!("Server Id: {}", server_id);
-
 }
